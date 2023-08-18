e879414f183692ff6cc367509931392688e186b6
Samarth <samarthbsacharya@gmail.com>
2023-08-18 12:40:36 +0530

    work

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // hello!
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app =express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req, res)=>{
+    res.send('This is the main page');
+})
+
+
+app.listen(8080, ()=>{
+    // res.send('new user has been createed');
+    console.log(`listening to server  8080`);
+})
